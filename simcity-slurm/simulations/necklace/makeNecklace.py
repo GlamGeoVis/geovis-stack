3eec8e7f2827c000addf28a40c8d4568d273be1e
Carlos Martinez <c.martinez@esciencecenter.nl>
2017-08-07 15:37:32 +0200

    Necklace demo

---
+++ simcity-slurm/simulations/necklace/makeNecklace.py
@@ -0,0 +1,30 @@
+import sys
+import json
+import numpy as np
+import urllib.request
+import subprocess
+
+inputsFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+inputs = json.load(open(inputsFile))
+
+baseUrl = 'http://nginx:8008/'
+agaJar = '/home/xenon/simulations/necklace/agalib-r1.jar'
+layerFile = 'input.geojson'
+columnName = 'size'
+
+x = float(inputs['centre'][0]['x'])
+y = float(inputs['centre'][0]['y'])
+r = float(inputs['radius'])
+layer = inputs['myDataLayer']
+layerUrl = baseUrl + layer
+
+print('WGET: ' + layerUrl)
+
+urllib.request.urlretrieve(layerUrl, filename=layerFile)
+
+javaLine = 'java -jar ' + agaJar + ' --in-file ' + layerFile + ' --auto-guide ' + str(r) + ' --data-column ' + columnName + ' --out-file ' + outputFile
+print(javaLine)
+
+subprocess.call(javaLine.split())
---
+++ simcity-slurm/simulations/necklace/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/xenon/simcity/bin/activate
+python /home/xenon/simulations/necklace/makeNecklace.py $SIMCITY_IN/input.json $SIMCITY_OUT/test.geojson
